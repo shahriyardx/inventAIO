661ece6213c6c6344c5685808f59b2b30228c30e
shahriyardx <mdshahriyaralam552@gmail.com>
2022-11-17 03:09:04 +0600

    Fix guilds

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 prefix: str = "?"
-default_guild_ids = [720619856294051880]
+default_guild_ids = [720619856294051880, 1042509712681353238]
 cogs = ["product", "buy", "sell", "profit", "management", "capital", "outgoings"]
 currency = "€"
